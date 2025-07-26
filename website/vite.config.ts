5edc90b2135f7b5eecf9a9e1cf6450205ad75dd7
Mark Hording <markhording@gmail.com>
2025-07-26 15:49:08 +0700

    website

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,40 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    proxy: {
-      '/api/chat': {
-        target: 'http://localhost:8001',
-        changeOrigin: true
-      },
-    },
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    target: 'es2015',
-    minify: 'esbuild',
-  },
-  esbuild: {
-    target: 'es2015',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2015',
-    },
-  },
-  define: {
-    'process.env.ROLLUP_SKIP_NATIVE': JSON.stringify('true'),
-    'process.env.ESBUILD_BINARY_PATH': JSON.stringify(''),
-  },
-}));
+});
